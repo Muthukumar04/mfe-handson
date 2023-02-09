12309c3b0b4f050317c97ce95d6f4b68fcf62aae
Muthukumar M <muthukumar@kissflow.com>
2023-02-09 10:05:25 +0530

    app 1 changes

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -1,12 +1,21 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const {ModuleFederationPlugin} = require('webpack').container
+const packageJson = require('./package.json')
 module.exports = {
   output: {
     path: path.join(__dirname, "/dist"), // the bundle output path
-    filename: "bundle.js", // the name of the bundle
+    filename: "app1.js", // the name of the bundle
   },
   plugins: [
+    new ModuleFederationPlugin({
+        name : 'app1',
+        filename:'remoteEntry.js',
+        exposes : {
+          './App1' : './src/App.js'
+        },
+        shared: packageJson.dependencies
+    }),
     new HtmlWebpackPlugin({
       template: "src/index.html", // to import index.html file inside index.js
     }),
